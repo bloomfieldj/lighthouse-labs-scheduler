01760c4797057fccbbf34bb8888363d9d2305b20
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-10-16 21:15:36 -0400

    Added validation for prop types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,10 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types"
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss"
 
-
-
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -23,3 +22,10 @@ export default function InterviewerList(props) {
     </section>
   )
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired
+};
+
+
+export default InterviewerList;
