3c29508171e0a6a9032daf947e551ad4f3a34b0b
Jonathon Bloomfield <bloomfield.codes@gmail.com>
2021-10-18 23:12:13 -0400

    indirectly change state and reduce redudant code

--- src/components/hooks/useVisualMode.js
+++ src/components/hooks/useVisualMode.js
@@ -4,47 +4,17 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  // const transition = function (newMode, replace = false) {
-  //   if (replace) {
-  //     setMode(newMode);
-  //     setHistory(prev => ([...prev, newMode]));
-  //   } else {
-  //     setMode(newMode);
-  //     setHistory(prev => ([...prev]));
-  //   };
-  //   console.log(newMode);
-  //   console.log(mode);
-  //   console.log(history);
-  // };
-
-  // const back = function () {
-  //   if (history.length === 1) {
-  //     transition((history[history.length - 1]), true)
-  //   }
-  //   if (history.length > 1) {
-  //     // setHistory(prev => [...prev]);
-  //     // setMode(history[history.length - 1]);
-  //     // setMode(SECOND)
-  //     // transition((history[history.length - 1]), true)
-  //     setMode(history[history.length - 1])
-
-  //   };
-  // };
-
   const transition = function (newMode, replace = false) {
     if (replace) {
       setMode(newMode);
       setHistory(prev => ([...prev, newMode]));
     } else {
-      setHistory(history => ([...history, newMode]));
       setMode(newMode);
+      setHistory(history => ([...history, newMode]));
     };
   };
 
   const back = function () {
-    if (history.length === 1) {
-      setMode(history[history.length - 1])
-    }
     if (history.length > 1) {
       let oldHistory = [...history];
       oldHistory.pop()
